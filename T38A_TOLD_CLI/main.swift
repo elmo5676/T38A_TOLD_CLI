a71ca9845280dd3e7e081f6e201f597f526eeebd
elmo <elmo5676@gmail.com>
2018-10-24 15:30:26 +0200

    Initial Commit

---
+++ T38A_TOLD_CLI/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  T38A_TOLD_CLI
+//
+//  Created by Matthew Elmore on 10/24/18.
+//  Copyright © 2018 Matthew Elmore. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
