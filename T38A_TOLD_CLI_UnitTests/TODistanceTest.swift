a18ac960af0d8417ee27ebbe4ee9c6d5507867c5
elmo <elmo5676@gmail.com>
2018-10-25 07:26:51 +0200

    Take Off Distance testing written

--- T38A_TOLD_CLI_UnitTests/TODistanceTest.swift
+++ T38A_TOLD_CLI_UnitTests/TODistanceTest.swift
@@ -24,89 +24,33 @@ class TODistanceTest: XCTestCase {
     override func tearDown() {
     }
     
-    //Specific Array
+    // MARK: - Tab Data
+    //Specific array
     func testTODistance_0Ft_N10KtsWindSpeed() {
-        let gonkTakeOffDistance = GonkTOData(TOData: td.TODist)
+        let gonkulator = GonkTOData(TOData: td.TODist)
         var result = 0.0
-//        let outPutArray = td.TODist_Temp_0K_N10KT
         let outPutArray = td.TODist[0][0]
-        var resultArray: [Double] = []
         for i in 0...9 {
             let temp = tempInput[i]
-            result = gonkTakeOffDistance.takeOffData(tempC: temp, windSpeed: -10, alt: 0)
-            resultArray.append(result)
+            result = gonkulator.takeOffData(tempC: temp, windSpeed: -10, alt: 0)
             XCTAssert(result == outPutArray[i], "Take Off Distance Calculated incorrectly")
-        }
-        print(resultArray)
-    }
+        }}
     
     //Specific Altitude
     func testTODistance_0Ft() {
-        let gonkTakeOffDistance = GonkTOData(TOData: td.TODist)
+        let gonkulator = GonkTOData(TOData: td.TODist)
         for j in 0...4 {
-//            let windSpdArray = td.TODist_0K[j]
             let windSpdArray = td.TODist[0][j]
             var result = 0.0
-            var resultArray: [Double] = []
             for i in 0...9 {
                 let temp = tempInput[i]
-                result = gonkTakeOffDistance.takeOffData(tempC: temp, windSpeed: windSpeedInput[j], alt: 0)
-                resultArray.append(result)
+                result = gonkulator.takeOffData(tempC: temp, windSpeed: windSpeedInput[j], alt: 0)
                 XCTAssert(result == windSpdArray[i], "Take Off Distance Calculated incorrectly")
-            }}
-        
-    }
-    
-    
-    
-    
-    
-    //Interpolated Values For specific Array
-    func testTODistanceInterpolated_0Ft_N10KtsWindSpeed() {
-        let gonkTakeOffDistance = GonkTOData(TOData: td.TODist)
-        var result = 0.0
-//        let outPutArray = td.TODist_Temp_0K_N10KT
-        let outPutArray = td.TODist[0][0]
-        var resultArray: [Double] = []
-        for i in 0...8 {
-            let temp = tempInputTest[i]
-            result = gonkTakeOffDistance.takeOffData(tempC: temp, windSpeed: -10, alt: 0)
-            resultArray.append(result)
-            XCTAssert(result > outPutArray[i] && result < outPutArray[i + 1], "Take Off Distance Calculated incorrectly")
-        }
-        print(resultArray)
-    }
-    
-    //Interpolated Values at 0Ft Alt
-    func testTODistanceInterpolated_0Ft() {
-        let gonkTakeOffDistance = GonkTOData(TOData: td.TODist)
-        for j in 0...3 {
-//            let windSpdArray = td.TODist_0K[j]
-            let windSpdArray = td.TODist[0][j]
-//            let nextWindSpdArray = td.TODist_0K[j + 1]
-            let nextWindSpdArray = td.TODist[0][j + 1]
-            var result = 0.0
-            var resultArray: [Double] = []
-            for i in 0...8 {
-                let temp = tempInput[i]
-                result = gonkTakeOffDistance.takeOffData(tempC: temp, windSpeed: windSpeedInputTest[j], alt: 0)
-                resultArray.append(result)
-                XCTAssert(result < windSpdArray[i] && result > nextWindSpdArray[i], "Take Off Distance Calculated incorrectly")
-            }}}
-    
-//    func testTODistanceInterpolated() {
-//        let gonkTakeOffDistance = GonkTOData(TOData: td.TODist)
-//        var result = 0.0
-//        var resultArray: [Double] = []
-//        
-//        
-//    }
-    
-    
+    }}}
     
     //All Tab Data Entries:
     func testTODistance() {
-        let gonkTakeOffDistance = GonkTOData(TOData: td.TODist)
+        let gonkulator = GonkTOData(TOData: td.TODist)
         var result = 0.0
         var resultArray: [Double] = []
         for k in 0...5 {
@@ -115,7 +59,7 @@ class TODistanceTest: XCTestCase {
                 let windSpdArray = TODistArray[j]
                 for i in 0...9 {
                     let temp = tempInput[i]
-                    result = gonkTakeOffDistance.takeOffData(tempC: temp, windSpeed: windSpeedInput[j], alt: altInput[k])
+                    result = gonkulator.takeOffData(tempC: temp, windSpeed: windSpeedInput[j], alt: altInput[k])
                     resultArray.append(result)
                     XCTAssert(result == windSpdArray[i], "Take Off Distance Calculated incorrectly")
                 }}}
@@ -124,9 +68,59 @@ class TODistanceTest: XCTestCase {
     }
     
     
+    // MARK: - Interpolated Values
+    //Interpolated Values For specific Array
+    func testTODistanceInterpolated_0Ft_N10KtsWindSpeed() {
+        let gonkulator = GonkTOData(TOData: td.TODist)
+        var result = 0.0
+        let outPutArray = td.TODist[0][0]
+        var resultArray: [Double] = []
+        for i in 0...8 {
+            let temp = tempInputTest[i]
+            result = gonkulator.takeOffData(tempC: temp, windSpeed: -10, alt: 0)
+            resultArray.append(result)
+            XCTAssert(result > outPutArray[i] && result < outPutArray[i + 1], "Take Off Distance Calculated incorrectly")
+        }
+        print(resultArray)
+    }
     
+    //Interpolated Values at 0Ft Alt
+    func testTODistanceInterpolated_0Ft() {
+        let gonkulator = GonkTOData(TOData: td.TODist)
+        for j in 0...3 {
+            let windSpdArray = td.TODist[0][j]
+            let nextWindSpdArray = td.TODist[0][j + 1]
+            var result = 0.0
+            for i in 0...9 {
+                let temp = tempInput[i]
+                result = gonkulator.takeOffData(tempC: temp, windSpeed: windSpeedInputTest[j], alt: 0)
+                XCTAssert(result < windSpdArray[i] && result > nextWindSpdArray[i], "Take Off Distance Calculated incorrectly")
+            }}}
     
-
-
-
+    //Interpolated Values at all Altitudes
+    func testTODistanceInterpolated_KFt() {
+        for k in 0...6 {
+            let gonkulator = GonkTOData(TOData: td.TODist)
+            for j in 0...3 {
+                let windSpdArray = td.TODist[k][j]
+                let nextWindSpdArray = td.TODist[k][j + 1]
+                var result = 0.0
+                for i in 0...9 {
+                    let temp = tempInput[i]
+                    result = gonkulator.takeOffData(tempC: temp, windSpeed: windSpeedInputTest[j], alt: Double(k))
+                    XCTAssert(result < windSpdArray[i] && result > nextWindSpdArray[i], "Take Off Distance Calculated incorrectly")
+    }}}}
+    
+    func testTODistanceInterpolated() {
+        let gonkulator = GonkTOData(TOData: td.TODist)
+        var result = 0.0
+        for k in 0...4 {
+            let toDistArray = td.TODist[k]
+            let nextToDistArray = td.TODist[k + 1]
+                for j in 0...4 {
+                    for i in 0...9 {
+                        result = gonkulator.takeOffData(tempC: tempInput[i], windSpeed: windSpeedInput[j], alt: altInputTest[k])
+                        XCTAssert(result > toDistArray[j][i] && result < nextToDistArray[j][i], "Take Off Distance Calculated incorrectly")
+    }}}}
+    
 }
